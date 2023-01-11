f05878cf098eb6e159b5b8dffec9fc1ce45304cb
Ifeanyi222 <linusify@ymail.com>
2023-01-10 23:47:36 -0800

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,11 @@
 #this manifest create a file at /tmp
 
-file {'/tmp/school':
-	ensure => 'file',
-	owner => 'www-data',
-	group => 'www-data',
-	mode => '0744',
-	content =>'I love Puppet',
+file {'resource title':
+	path=>'/tmp/school',
+	ensure=> 'file',
+	owner=> 'www-data',
+	group=> 'www-data',
+	mode=> '0744',
+	content=>'I love Puppet',
 	
 }
